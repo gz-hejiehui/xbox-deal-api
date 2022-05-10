bb1b360473f14b5396b569e84d263103c2fb2f38
gz-hejiehui <gz-hejiehui@outlook.com>
2022-05-10 11:42:56 +0800

    Update the usage example case

--- example/basic.php
+++ example/basic.php
@@ -1,10 +1,13 @@
 <?php
 
 use GzHejiehui\XboxDealApi\ClientBuilder;
+use GzHejiehui\XboxDealApi\Options;
+use GzHejiehui\XboxDealApi\Sdk;
 use Http\Client\Common\Plugin\HeaderDefaultsPlugin;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+// Customize the http client
 $clientBuilder = new ClientBuilder();
 $clientBuilder->addPlugins(
     new HeaderDefaultsPlugin([
@@ -12,10 +15,29 @@ $clientBuilder->addPlugins(
     ])
 );
 
-$options = new \GzHejiehui\XboxDealApi\Options([
+// Create the SDK instance
+$sdk = new Sdk(new Options([
     'client_builder' => $clientBuilder,
-]);
+]));
 
-$sdk = new \GzHejiehui\XboxDealApi\Sdk($options);
-//print_r($sdk->channel('TopFree')->language('zh-HK')->skipItems(116)->get());
-print_r($sdk->product()->bigIds('C48ZFTBQ17Q3')->get());
+// Get items of the channel
+try {
+    $data = $sdk->channel()->name('TopFree')->language('zh-HK')->skipItems(10)->get();
+    foreach ($data['Items'] as $item) {
+        // TODO: Do something with the item
+        continue;
+    }
+} catch (\Http\Client\Exception $e) {
+    echo $e->getMessage();
+}
+
+// Get information of the products
+try {
+    $data = $sdk->product()->bigIds('C48ZFTBQ17Q3', '9NQQ8B4PJR25')->get();
+    foreach ($data['Products'] as $item) {
+        // TODO: Do something with the item
+        continue;
+    }
+} catch (\Http\Client\Exception $e) {
+    echo $e->getMessage();
+}
